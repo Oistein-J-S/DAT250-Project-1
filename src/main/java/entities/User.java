e85eb104923f6eb3649c156f0d575981de37d65f
148913 <148913@stud.hvl.no>
2018-09-25 17:22:28 +0200

    Refractored file structure

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,66 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Dat250
+ *  
+ * Persistent class for the User database table.
+ * Class defining the main system user entity
+ */
+
+@Entity
+@XmlRootElement
+@Table(name="user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L; //Object version controll
+	
+	/**Class variables*/
+	//Create elements ids automatically, incremented 1 by 1
+	@TableGenerator(
+	name = "myTableGenerator",
+	allocationSize = 1,
+	initialValue = 1)
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE,generator="yourTableGenerator")
+	private int id;
+	
+	private String eMail;
+	private String name;
+	private String phone;
+	//private Product[] products;
+	//private Bid[] bids;
+	
+	/**Constructor*/
+	public User() {};
+	public User(String eMail, String name, String phone) {
+		this.eMail = eMail;
+		this.name = name;
+		this.phone = phone;
+	};
+	
+	/**Class services*/
+	
+	//public String[] productReviews(Product ID) {
+	public String[] productReviews() {
+		return null;
+	}
+	
+	/**Helper methodes*/
+	private boolean isValidEMail(String eMail) {
+		// TODO create method body
+		return false;
+	};
+	private boolean isValidName(String name) {
+		// TODO create method body
+		return false;
+	};
+	private boolean isValidPhone(String phone) {
+		// TODO create method body
+		return false;
+	};
+
+}
